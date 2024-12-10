3492ac42ad4dbddc184b11c4f332b1072a14c4ab
Hovah Yii <hovahyii@gmail.com>
2024-12-10 16:20:35 +0800

    fix small bugs
    Signed-off-by: Hovah Yii <hovahyii@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="min-h-screen flex flex-col">
-    <!-- Full-width Navbar -->
-    <Navbar />
+
 
     <!-- Main Content -->
     <main class="flex-grow px-4 py-6">
       <router-view />
     </main>
 
-    <!-- Full-width Footer -->
-    <Footer />
   </div>
 </template>
 
